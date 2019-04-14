c8380e9e163aca6692d5e3f253630a808cc3258c
MillicentNj <millicent.njuguna@allangray.co.za>
2019-04-14 19:22:48 +0300

    chore: add a logger middleware morgan

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-require("dotenv").config(); //loads environment variables from a .env file into process.env
 const app = express();
+const morgan = require("morgan");
+
+require("dotenv").config(); //loads environment variables from a .env file into process.env
+app.use(morgan('combined'))
 
 const PORT = process.env.PORT || 4001;
+
+app.get("/", (req, res, next) => {
+  res.send(["Millllyy welcome"]).status(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
