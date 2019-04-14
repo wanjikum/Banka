3b3ec5b860d165ea0adf769c99fd711a57bd3f77
MillicentNj <millicent.njuguna@allangray.co.za>
2019-04-14 19:06:25 +0300

    chore: Add configuration files

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
