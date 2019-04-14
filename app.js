7c538ace1099352425db31940121ced38527e978
MillicentNj <millicent.njuguna@allangray.co.za>
2019-04-14 20:02:42 +0300

    chore: Add Eslint Airbnb style

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
-require("dotenv").config(); //loads environment variables from a .env file into process.env
-const morgan = require("morgan"); // logger
-const bodyParser = require("body-parser"); //parses your request and converts it into a format from which you can easily extract relevant information that you may need.
+require('dotenv').config(); // loads environment variables from a .env file into process.env
+const morgan = require('morgan'); // logger
+const bodyParser = require('body-parser'); // parses your request and converts it into a format from which you can easily extract relevant information that you may need.
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 4001;
 
-app.get("/", (req, res, next) => {
-  res.send(["Millllyy welcome"]).status(200);
+app.get('/', (req, res, next) => {
+  res.send(['Millllyy welcome']).status(200);
 });
 
 app.listen(PORT, () => {
