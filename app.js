8854c1de6968193d638a4904769c62991596cbdf
MillicentNj <millicent.njuguna@allangray.co.za>
2019-04-14 19:42:56 +0300

    chore: Add body parser

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 
 require("dotenv").config(); //loads environment variables from a .env file into process.env
-app.use(morgan('combined'))
+const morgan = require("morgan"); // logger
+const bodyParser = require("body-parser"); //parses your request and converts it into a format from which you can easily extract relevant information that you may need.
+
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 4001;
 
